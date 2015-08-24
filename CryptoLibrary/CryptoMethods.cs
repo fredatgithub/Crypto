a068c4ad7dbd021665ee6d9c3ba9ff588f753c11
Freddy Juhel <domaine75@gmail.com>
2015-08-24 17:21:17 +0200

    Add crypto library project

--- CryptoLibrary/CryptoMethods.cs
+++ CryptoLibrary/CryptoMethods.cs
@@ -17,53 +17,34 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-using System;
-namespace SearchAllCombinaisons
+
+using System.Security.Cryptography.X509Certificates;
+
+namespace CryptoLibrary
 {
-  public class Program
+  public static class CryptoMethods
   {
-    private static readonly char[] PossibleCharacters =  {'0','1','2','3','4','5','6','7','8','9',
+    public static readonly char[] PossibleCharacters =  {'0','1','2','3','4','5','6','7','8','9',
       'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
       'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
       '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}',
       ';', ':', ',', '.', '<', '>', '/', '?' };
-    private static string _findPassword;
-    private static int _combinaisons;
 
-    public static void Main()
+    public static char[] SortPossibleCharacters(Language selectedLanguage)
     {
-      int count;
-      Console.WriteLine("Enter your Password");
-      _findPassword = Console.ReadLine();
-      DateTime today = DateTime.Now;
-      Console.WriteLine("START:\t{0}", today.ToString("yyyy-MM-dd_HH:mm:ss"));
-      for (count = 0; count <= 15; count++)
-      {
-        Recurse(count, 0, "");
-      }
+      char[] result;
+      result = PossibleCharacters;
+      // TODO write code to implement sorting according to selectedLanguage letter frequencies
+      return result;
     }
+  }
 
-    private static void Recurse(int length, int position, string baseString)
-    {
-      for (int count = 0; count < PossibleCharacters.Length; count++)
-      {
-        _combinaisons++;
-        if (position < length - 1)
-        {
-          Recurse(length, position + 1, baseString + PossibleCharacters[count]);
-        }
-
-        if (baseString + PossibleCharacters[count] == _findPassword)
-        {
-          DateTime today = DateTime.Now;
-          Console.WriteLine("END:\t{0}\nNumber of combinaisons:\t{1}", today.ToString("yyyy-MM-dd_HH:mm:ss"), _combinaisons);
-          Console.WriteLine("Press a key to exit:");
-          Console.ReadKey();
-          Environment.Exit(0);
-        }
-
-        Console.WriteLine(_combinaisons);
-      }
-    }
+  public enum Language
+  {
+    NoLanguageSpecified,
+    English,
+    DefaultLanguage = English,
+    French,
+    OtherLanguage
   }
 }
