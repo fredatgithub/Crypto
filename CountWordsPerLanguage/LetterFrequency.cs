8d640f1e68513eea8ec83dfef49663b02a2985e7
Freddy Juhel <domaine75@gmail.com>
2015-09-06 14:04:41 +0200

    Add class

---
+++ CountWordsPerLanguage/LetterFrequency.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Policy;
+
+namespace CountWordsPerLanguage
+{
+  class LetterFrequency
+  {
+    public string Language { get; set; }
+    public Hash FileHashHash { get; set; }
+    public int LetterA { get; set; }
+    public int LetterB { get; set; }
+    public int LetterC { get; set; }
+    public int LetterD { get; set; }
+    public int LetterE { get; set; }
+    public int LetterF { get; set; }
+    public int LetterG { get; set; }
+    public int LetterH { get; set; }
+    public int LetterI { get; set; }
+    public int LetterJ { get; set; }
+    public int LetterK { get; set; }
+    public int LetterL { get; set; }
+    public int LetterM { get; set; }
+    public int LetterN { get; set; }
+    public int LetterO { get; set; }
+    public int LetterP { get; set; }
+    public int LetterQ { get; set; }
+    public int LetterR { get; set; }
+    public int LetterS { get; set; }
+    public int LetterT { get; set; }
+    public int LetterU { get; set; }
+    public int LetterV { get; set; }
+    public int LetterW { get; set; }
+    public int LetterX { get; set; }
+    public int LetterY { get; set; }
+    public int LetterZ { get; set; }
+
+  }
+}
