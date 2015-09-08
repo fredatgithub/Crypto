6eba8787f8606ba6e8912af96b33e5c7b1982914
Freddy Juhel <domaine75@gmail.com>
2015-09-08 13:08:30 +0200

    change initial window height and width to 800x600

--- CountWordsPerLanguage/Properties/Settings.Designer.cs
+++ CountWordsPerLanguage/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace CountWordsPerLanguage.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("200")]
+        [global::System.Configuration.DefaultSettingValueAttribute("800")]
         public int WindowHeight {
             get {
                 return ((int)(this["WindowHeight"]));
@@ -37,7 +37,7 @@ namespace CountWordsPerLanguage.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("200")]
+        [global::System.Configuration.DefaultSettingValueAttribute("600")]
         public int WindowWidth {
             get {
                 return ((int)(this["WindowWidth"]));
